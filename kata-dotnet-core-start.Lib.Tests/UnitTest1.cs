b8a41f515ad81a6fa794b2c9fae4bdf8d5b69ea6
Mohamed Reddahi <mohamed.reddahi@gantner.com>
2022-11-22 10:42:18 +0100

    Swap #1

--- kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
+++ kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
@@ -3,8 +3,32 @@ namespace kata_dotnet_core_start.Lib.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Generate_sequence_100_integers()
     {
-        Assert.Equal(true, false);
+        var list = Class1.Generate();
+        Assert.Equal(100, list.Count);
+    }
+    
+    [Fact]
+    public void Sequence_starts_at_1()
+    {
+        var list = Class1.Generate();
+        Assert.Equal(1, list.First());
+    }
+    
+    [Fact]
+    public void Sequence_ends_at_100()
+    {
+        var list = Class1.Generate();
+        Assert.Equal(100, list.Last());
+    }
+    
+    [Fact]
+    public void FizzBuzz_sequence_prints_Fizz_at_3()
+    {
+        var list = Class1.Generate();
+        var seq = Class1.FizzBuzzify(list);
+
+        Assert.Equal("Fizz", seq[2]);
     }
 }
--- kata-dotnet-core-start.Lib/Class1.cs
+++ kata-dotnet-core-start.Lib/Class1.cs
@@ -1,5 +1,13 @@
 ﻿namespace kata_dotnet_core_start.Lib;
 public class Class1
 {
+    public static List<int> Generate()
+    {
+        return Enumerable.Range(1, 100).ToList();
+    }
 
+    public static List<string> FizzBuzzify(List<int> list)
+    {
+        return list.Select(i => i.ToString()).ToList();
+    }
 }
